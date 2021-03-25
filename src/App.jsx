d29d7c36b219a3568dfd520963a0eef52c20e0a5
viktor <viktorkarlsson125@gmail.com>
2021-03-25 19:17:11 +0100

    adding images, videos and style components for title, subtitle and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React from "react"
+import React from 'react'
 import Jumbotron from './components/jumbotron'
 import jumboData from './fixtures/jumbo.json'
 
 const App = () => {
   return (
-    <div >
+    <div>
       <Jumbotron.Container>
         {jumboData.map((item) => (
           <Jumbotron key={item.id} direction={item.direction}>
-            <p>{item.title}</p>
-            <p>{item.subTitle}</p>
-            <p>{item.image}</p>
-            <p>{item.alt}</p>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
