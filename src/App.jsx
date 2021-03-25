b439da9ee0780221ea75e298edc811ade9a85a2e
viktor <viktorkarlsson125@gmail.com>
2021-03-25 20:54:19 +0100

    creating a footer with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FooterContainer } from './containers/footer'
 import JumbotronContainer from './containers/jumbotronContainer'
 
 const App = () => {
   return (
-    <JumbotronContainer />
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   )
 }
 
