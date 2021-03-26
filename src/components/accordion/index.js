9fce9f7cc068cbe65cd3d3dfd55f0b11454c4a5d
viktor <viktorkarlsson125@gmail.com>
2021-03-26 19:51:35 +0100

    adding style to faq

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,5 +1,13 @@
 import React, { createContext, useState, useContext } from 'react'
-import {Container, Frame, Title, Item, Inner, Header, Body} from './styles/accordion'
+import {
+  Container,
+  Frame,
+  Title,
+  Item,
+  Inner,
+  Header,
+  Body,
+} from './styles/accordion'
 
 const ToggleContext = createContext()
 
@@ -15,10 +23,6 @@ Accordion.Title = function AccordionTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>
 }
 
-Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
-  return <Frame {...restProps}>{children}</Frame>
-}
-
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
   const [toggleShow, setToggleShow] = useState(false)
   return (
@@ -29,19 +33,24 @@ Accordion.Item = function AccordionItem({ children, ...restProps }) {
 }
 
 Accordion.Header = function AccordionHeader({ children, ...restProps }) {
-  const {toggleShow, setToggleShow} = useContext(ToggleContext)
+  const { toggleShow, setToggleShow } = useContext(ToggleContext)
   return (
-    <Header 
-      onClick={() => setToggleShow((toggleShow) => !toggleShow)} 
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
       {...restProps}
     >
       {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
     </Header>
   )
 }
 
-Accordion.Body = function AccordionBody({children, ...restProps}) {
-  const {toggleShow} = useContext(ToggleContext)
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext)
 
   return toggleShow ? <Body {...restProps}>{children}</Body> : null
 }
