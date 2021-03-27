9d911431e22995883e4b51afe0db5e3303c79d88
viktor <viktorkarlsson125@gmail.com>
2021-03-27 16:01:18 +0100

    header done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,11 +3,28 @@ import { FaqsContainer } from '../containers/faqs'
 import { FooterContainer } from '../containers/footer'
 import HeaderContainer from '../containers/header'
 import JumbotronContainer from '../containers/jumbotronContainer'
+import { Feature, OptForm } from '../components'
 
 const Home = () => {
   return (
     <>
-      <HeaderContainer/>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited Films, TV Programs and More</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Enter Email" />
+            <OptForm.Button>Try it Now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your Email just for fun. You will not gain
+              access
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
