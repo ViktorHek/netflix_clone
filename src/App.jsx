0c1dafdc14a476650acd444558fcd4c360f52148
viktor <viktorkarlsson125@gmail.com>
2021-04-09 17:20:39 +0200

    creating secure routes with redirections

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
 import * as ROUTES from './constants/routes'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Home, Browse, Signin, Signup } from './pages'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 const App = () => {
+  const user = null
+  
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home/>
-      </Route>
-      <Route exact path="/browse">
-        <Browse/>
-      </Route>
-      <Route exact path="/signup">
-        <Signup/>
-      </Route>
-      <Route exact path="/signin">
-        <Signin/>
-      </Route>
+      <Switch>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exact
+        >
+          <Home />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+        >
+          <Signup />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   )
 }
