b341f7a0b0a912fa90ed876f5bb456b40ab41e86
viktor <viktorkarlsson125@gmail.com>
2021-04-19 17:16:23 +0200

    creating custom hook useContent

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import {useContent} from '../hooks'
 
 const Browse = () => {
+
+  const {series } = useContent('series')
+  console.log(series)
+
   return (
     <>
       <p>Browse</p>
