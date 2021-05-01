6b81b40f04e3b8910d50cf17fc8466ba4897ccf6
viktor <viktorkarlsson125@gmail.com>
2021-05-01 19:03:09 +0200

    creating loading component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
+import { Loading, Header } from '../components'
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({})
@@ -15,5 +16,14 @@ export function BrowseContainer({ slides }) {
     }, 3000)
   }, [profile.displayName])
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1">
+        <h1>Hello</h1>
+      </Header>
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  )
 }
