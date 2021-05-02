16577720141cb0e9f51ddca2aad727c28ffcd0ee
viktor <viktorkarlsson125@gmail.com>
2021-05-02 17:19:14 +0200

    adding sign out on browser

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -14,6 +14,22 @@ export const Background = styled.div`
   }
 `
 
+export const Link = styled.p`
+  color: white;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
+
 export const Group = styled.div`
   display: flex;
   align-items: center;
@@ -38,13 +54,13 @@ export const Container = styled.div`
   }
 `
 export const Picture = styled.button`
-  background: url(${({src}) => src});
+  background: url(${({ src }) => src});
   background-size: contain;
   border: 0;
   width: 32px;
   height: 32px;
   cursor: pointer;
-`;
+`
 
 export const Dropdown = styled.div`
   display: none;
@@ -54,7 +70,33 @@ export const Dropdown = styled.div`
   width: 100px;
   top: 32px;
   right: 10px;
-`;
+
+  ${Group}:last-of-type ${Link} {
+    cursor: pointer;
+  }
+
+  ${Group} {
+    margin-bottom: 10px;
+
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+
+    ${Link}, ${Picture} {
+      cursor: default;
+    }
+  }
+
+  button {
+    margin-right: 10px;
+  }
+
+  p {
+    font-size: 12px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+`
 
 export const Profile = styled.div`
   display: flex;
@@ -70,7 +112,7 @@ export const Profile = styled.div`
     display: flex;
     flex-direction: column;
   }
-`;
+`
 
 export const Logo = styled.img`
   height: 32px;
@@ -119,22 +161,6 @@ export const Text = styled.p`
   line-height: normal;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
   margin: 0;
-`
-
-export const Link = styled.p`
-  color: white;
-  text-decoration: none;
-  margin-right: 30px;
-  font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
-  cursor: pointer;
-
-  &:hover {
-    font-weight: bold;
-  }
-
-  &:last-of-type {
-    margin-right: 0;
-  }
 `
 
 export const FeatureCallOut = styled.p`
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -34,9 +34,10 @@ export function BrowseContainer({ slides }) {
               <Header.Dropdown>
                 <Header.Group>
                   <Header.Picture src={user.photoURL} />
-                  <Header.TextLink>
-                    {user.displayName}
-                  </Header.TextLink>
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()} >Sign Out</Header.TextLink>
                 </Header.Group>
               </Header.Dropdown>
             </Header.Profile>
